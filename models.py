dfe02804da0c97ae0a3035b9adb32f98926386f2
shaliniV7675 <dsri78304@gmail.com>
2026-08-08 21:29:26 +0530

    Add files via upload

--- models.py
+++ models.py
@@ -1,11 +1,11 @@
 from django.db import models
-class User(models.Model):
-    full_name=models.CharField(max_length=100)
-    phone=models.CharField(max_length=10)
 
-    email=models.EmailField(unique=True)
-    password=models.CharField(max_length=100)
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField()
+    image = models.ImageField(upload_to='products/', blank=True, null=True)
+
     def __str__(self):
-        return self.email
-
-# Create your models here.
+        return self.name
+    
